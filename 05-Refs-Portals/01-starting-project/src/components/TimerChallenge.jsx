215be762c4633558a6d44fba1776cada695c3ec5
maisamsaleh134 <maisamsaleh134@gmail.com>
2025-06-19 19:32:18 +0300

    vid 140_141: using useRef and forwardRef to show modal(popup)

--- 05-Refs-Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ 05-Refs-Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -2,16 +2,23 @@ import { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }){
+    const dialog = useRef();
     const timer = useRef();
     const [ timerStarted, setTimerStarted ] = useState(false);
     const [ timerExpired, setTimerExpired ] = useState(false);
 
-    function handleStart(){
+    function showPoppup(){
+        if(timerExpired){
+            dialog.current.showModal();
+        }
+        console.log(timerExpired)
+    }
+
+    async function handleStart(){
+        setTimerStarted(true);
         timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
-        
-        setTimerStarted(true);
     }
 
     function handleStop(){
@@ -20,14 +27,14 @@ export default function TimerChallenge({ title, targetTime }){
 
     return(
         <>
-            {timerExpired && <ResultModal targetTime={targetTime} result={'lost'} />}
+            {timerExpired && <ResultModal ref={dialog} targetTime={targetTime} result={'lost'} />}
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
                     {targetTime} second{targetTime>1 ? 's' : ''}
                 </p>
                 <p>
-                    <button onClick={timerStarted ? handleStop : handleStart}>
+                    <button onClick={timerStarted ? handleStop : async ()=>{ await handleStart(); showPoppup;}}>
                         {timerStarted ? 'Stop' : 'Start'} Challenge 
                     </button>
                 </p>
