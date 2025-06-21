a94a3020aa7029c5b5ac488c947394c3de83e2ef
maisamsaleh134 <maisamsaleh134@gmail.com>
2025-06-21 20:01:34 +0300

    vid 154: switching between noProjectSelected and NewProject components

--- 06-practice-project2/01-starting-project/src/App.jsx
+++ 06-practice-project2/01-starting-project/src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectSidebar from './components/ProjectSidebar.jsx'
 
 function App() {
+  const [ projectState, setProjectState ] = useState({
+    selectedProjectId: undefined,
+    pojects: []
+  })
+
+  function handleStartAddPoject(){
+    setProjectState((prevState)=>{
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+  if(projectState.selectedProjectId === null){
+    content = <NewProject />
+  }else if(projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddPoject} />
+  }
+
   return (
     <main className='h-screen my-8 flex py-8'>
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddPoject} />
+      {content}
     </main>
   );
 }
