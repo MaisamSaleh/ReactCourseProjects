b577044f5d13035da9f3ab42c77d9b6ef875e48a
maisamsaleh134 <maisamsaleh134@gmail.com>
2025-06-21 12:53:30 +0300

    vid 151: adding NewProject component

--- 06-practice-project2/01-starting-project/src/App.jsx
+++ 06-practice-project2/01-starting-project/src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from './components/NewProject.jsx';
 import ProjectSidebar from './components/ProjectSidebar.jsx'
 
 function App() {
   return (
-    <main className='h-screen my-8'>
+    <main className='h-screen my-8 flex py-8'>
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
---
+++ 06-practice-project2/01-starting-project/src/components/NewProject.jsx
@@ -0,0 +1,17 @@
+import Input from "./Input";
+
+export default function NewProject(){
+    return(
+        <div>
+            <menu>
+                <li><button>Cancle</button></li>
+                <li><button>Save</button></li>
+            </menu>
+            <div>
+                <Input label='Title' />
+                <Input label='Description' textarea />
+                <Input label='Due Time' />
+            </div>
+        </div>
+    );
+}
