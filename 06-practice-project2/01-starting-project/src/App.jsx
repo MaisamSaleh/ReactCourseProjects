5abd6d1ffc1f0a9b3ea135f50f6ff56067bc8ad9
maisamsaleh134 <maisamsaleh134@gmail.com>
2025-06-23 18:32:34 +0300

    vid 155: getting input values by ref and forwardedRef

--- 06-practice-project2/01-starting-project/src/App.jsx
+++ 06-practice-project2/01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectSidebar from './components/ProjectSidebar.jsx'
@@ -6,7 +6,7 @@ import ProjectSidebar from './components/ProjectSidebar.jsx'
 function App() {
   const [ projectState, setProjectState ] = useState({
     selectedProjectId: undefined,
-    pojects: []
+    projects: []
   })
 
   function handleStartAddPoject(){
@@ -18,12 +18,24 @@ function App() {
     })
   }
 
+  function handleAddProject(projectData){
+    setProjectState((prevState)=>{
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+      return [...prevState.projects , newProject];
+    })
+  }
+
   let content;
   if(projectState.selectedProjectId === null){
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   }else if(projectState.selectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject={handleStartAddPoject} />
   }
+  
+  console.log(projectState);
 
   return (
     <main className='h-screen my-8 flex py-8'>
